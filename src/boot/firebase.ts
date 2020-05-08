a03e75842237dcb21d0729a8f98ba89dcdee2512
Timothy Logan <timtimmahh@gmail.com>
2020-05-08 11:11:32 -0400

    Initial commit

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -1,19 +1,8 @@
+import firebaseConfig from '../statics/firebase-config'
 import { auth, firestore, initializeApp } from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import { VueConstructor } from 'vue';
-
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAR_cgkMfVasxDSlj2pf787GwIR6rrcPEs',
-  authDomain: 'redcross-data.firebaseapp.com',
-  databaseURL: 'https://redcross-data.firebaseio.com',
-  projectId: 'redcross-data',
-  storageBucket: 'redcross-data.appspot.com',
-  messagingSenderId: '224457547724',
-  appId: '1:224457547724:web:0c603be11db9630d2d8769',
-  measurementId: 'G-B6SBY0J4FV'
-};
 
 declare module 'vue/types/vue' {
   interface Vue {
