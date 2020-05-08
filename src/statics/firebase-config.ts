9ae1e8c1b4a9b12c46dd9bcd8c533551554935c3
Timothy Logan <timtimmahh@gmail.com>
2020-05-08 11:14:15 -0400

    Initial commit

---
+++ src/statics/firebase-config.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'FIREBASE_API_KEY',
+  authDomain: 'FIREBASE_AUTH_DOMAIN',
+  databaseURL: 'FIREBASE_DATABASE_URL',
+  projectId: 'FIREBASE_PROJ_ID',
+  storageBucket: 'FIREBASE_STORAGE_BUCKET',
+  messagingSenderId: 'FIREBASE_SENDER_ID',
+  appId: 'FIREBASE_APP_ID',
+  measurementId: 'FIREBASE_MEASUREMENT_ID'
+};
+
+export default firebaseConfig;
